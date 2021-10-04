d40b7a248db0ede1ee2fec137cc71c28b3e3c1a7
lostinthestack <lostinthestack@protonmail.com>
2021-10-04 11:10:49 -0500

    init

---
+++ IterationStatements-Exercise01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace IterationStatements_Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instructions were to write the example loop a total of 15 times.
+            // I made an executive decision to just print it to the console 15 times
+            // instead of writing it once and copy-pasta in the IDE.
+
+            for (int i = 0; i < 15; i++)
+                ExampleLoop();
+        }
+        static void ExampleLoop()
+        {
+            var exampleLoop = new StringBuilder();
+            exampleLoop.AppendLine("for (int i = 0; i < 5; i++)");
+            exampleLoop.AppendLine("{");
+            exampleLoop.AppendLine("     Console.WriteLine($\"{i}\")");
+            exampleLoop.AppendLine("}");
+
+            Console.WriteLine(exampleLoop);
+        }
+    }
+}
